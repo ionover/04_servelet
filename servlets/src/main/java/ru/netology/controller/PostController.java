121ec2c39c6fc2d6dd483886e5df8ebe482ad295
v.ionov <v.ionov@mail.ru>
2025-06-02 10:16:43 +0300

    add by id actions

--- servlets/src/main/java/ru/netology/controller/PostController.java
+++ servlets/src/main/java/ru/netology/controller/PostController.java
@@ -9,33 +9,48 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public PostController(PostService service) {
+        this.service = service;
+    }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        try {
+            final var post = service.getById(id);
+            final var gson = new Gson();
+            response.getWriter().print(gson.toJson(post));
+        } catch (ru.netology.exception.NotFoundException e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        try {
+            service.getById(id);
+            service.removeById(id);
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (ru.netology.exception.NotFoundException e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 }
