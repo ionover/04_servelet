7440b2e785e78bdd9fa83d4d7835501675e22fc9
v.ionov <v.ionov@mail.ru>
2025-06-04 08:43:15 +0300

    now u can start it

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -16,7 +16,8 @@ public class MainServlet extends HttpServlet {
     @Override
     public void init() {
 
-        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        controller = new AnnotationConfigApplicationContext("ru.netology").getBean(PostController.class);
+
 //        final var repository = new PostRepository();
 //        final var service = new PostService(repository);
 //        controller = new PostController(service);
